8c64c602525a1b27b9b6727466cba8ed032acd7f
rceglie <rceglie@email.unc.edu>
2022-02-22 02:51:58 -0500

    basic button css

---
+++ src/components/Letters.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hello = () => {
+    return(
+        <div>
+            <button class="letter">S</button>
+            <button class="letter">T</button>
+            <button class="letter">O</button>
+            <button class="letter">R</button>
+            <button class="letter">E</button>
+        </div>
+    )
+}
+
+export default Hello

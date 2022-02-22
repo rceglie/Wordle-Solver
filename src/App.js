d1eca6830ba77fda8149603909e384ee6091f2e5
rceglie <rceglie@email.unc.edu>
2022-02-22 12:02:03 -0500

    tsx changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import Hello from './components/Letters'
 
 function App() {
+
+  var letterIsOpen
+
+  fun (() => {
+    letterIsOpen = false
+  })
+
+
+
   return (
     <div className="App">
       <h3>Hi</h3>
-      <Hello></Hello>
+      <Hello
+        isOpen={letterIsOpen}
+        />
     </div>
     // Initialize stuff
   );

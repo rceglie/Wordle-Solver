84bd74f6f1ea1bb6a791c040b906a8688e26f4b5
rceglie <rceglie@email.unc.edu>
2022-02-23 01:25:17 -0500

    wrote wordle solver code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import './App.css';
 import React from 'react'
+import {Calculate} from './Calculate.mjs'
 //import LetterRow from './components/Letters'
 
 function App() {
 
   const rows = [0,1,2,3,4]
 
+  const guess = ['','','','','']
+  const guessdata = ["","","","",""]
+
   var guessnum = 0
   var letternum = 0
 
-  const updateColors = () => {
+  const updateData = () => {
     for (var i = 0; i < 5; i++){
       var btn = document.getElementsByClassName(`row-${guessnum} letter-${i}`).item(0)
-      btn.style.backgroundColor = btn.getAttribute("data-color");
-      btn.style.color = "white"
+      guess[i] = btn.textContent
+      if (btn.style.backgroundColor == "rgb(58, 58, 60)"){
+        guessdata[i] = "gray"
+      } else if (btn.style.backgroundColor == "rgb(181, 159, 59)"){
+        guessdata[i] = "yellow"
+      } else if (btn.style.backgroundColor == "rgbrgb(83, 141, 78)"){
+        guessdata[i] = "green"
+      }
+      btn.setAttribute("disabled", "")
+      
     }
   }
 
-  function guess () {
-    updateColors()
+  function findWords () {
+    updateData()
+    console.log(guess)
+    console.log(guessdata)
+    console.log(Calculate(1, guess, guessdata, 0))
   }
 
-  function letterClick (row, letter) {
-    console.log(row, letter)
-    /*btn = ""
-    if (btn.style.color = "grey"){
-      btn.style.color = "yellow"
-    } else if (btn.style.color = "yellow"){
-      btn.style.color = "green"
-    } else {
-      btn.style.backgroundColor = "grey"
-    }*/
-  }
 
   document.addEventListener('keydown', function(event) {
     if (/^[A-Za-z]$/.test(event.key) && letternum < 5){
@@ -43,22 +47,39 @@ function App() {
     if (letternum > 0 && event.key === "Backspace"){
       var btn = document.getElementsByClassName(`row-${guessnum} letter-${letternum-1}`).item(0)
       btn.textContent = ""
-      btn.setAttribute("disabled", "")
       letternum--
+    }
+    if (letternum == 5 && event.key == "Enter"){
+      findWords()
     }
   })
 
   const LetterRow  = (row)  => {
 
-    var letters = [0,1,2,3,4]
+    function letterClick (r, letter) {
+      
+      var btn = document.getElementsByClassName(`row-${r} letter-${letter}`).item(0)
+      btn.style.color = "white"
+      console.log(btn.style.backgroundColor)
+      if (btn.style.backgroundColor == "rgb(58, 58, 60)"){
+        btn.style.backgroundColor = "rgb(181, 159, 59)"
+        btn.setAttribute("data-color", "yellow")
+      } else if (btn.style.backgroundColor == "rgb(181, 159, 59)"){
+        btn.style.backgroundColor = "rgb(83,141,78)"
+        btn.setAttribute("data-color", "green")
+      } else {
+        btn.style.backgroundColor = "rgb(58, 58, 60)"
+        btn.setAttribute("data-color", "gray")
+      }
+    }
 
     return(
       <div className="letter-row">
-        {letters.map((letter) => <button 
-          className={`letter row-${row} letter-${letter}`}
-          data-color="grey"
-          onClick={()=>letterClick.call(row, letter)}
-        ></button>)}
+        <button className={`letter row-${row} letter-0`} data-color="grey" onClick={()=>{letterClick(row,0)}}></button>
+        <button className={`letter row-${row} letter-1`} data-color="grey" onClick={()=>{letterClick(row,1)}}></button>
+        <button className={`letter row-${row} letter-2`} data-color="grey" onClick={()=>{letterClick(row,2)}}></button>
+        <button className={`letter row-${row} letter-3`} data-color="grey" onClick={()=>{letterClick(row,3)}}></button>
+        <button className={`letter row-${row} letter-4`} data-color="grey" onClick={()=>{letterClick(row,4)}}></button>
       </div>
     )
   }
@@ -68,7 +89,7 @@ function App() {
       <div className="letter-area">
         {rows.map((row) => LetterRow(row))}
         <div className="guess-area">
-          <button className="guess-btn" onClick={guess}>Find Words</button>
+          <button className="guess-btn" onClick={findWords}>Find Words</button>
         </div>
       </div>
     </div>
---
+++ src/Calculate.mjs
@@ -0,0 +1,114 @@
+import { WORDS } from './words_alpha.mjs'
+
+console.log(WORDS)
+
+var SCORES = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+          "x": 8, "z": 10}
+
+var allwords = []
+WORDS.forEach((word) => {
+    if (word.length == 5){
+        allwords.push(word)
+    }
+})
+
+console.log(Calculate(allwords, ['s','t',"o",'r','a'],["green", "green", "yellow", "gray", "gray"],0))
+
+export function Calculate(words, letters, data, unique){
+
+    var includes = []
+    var excludes = []
+    var correct = ['','','','','']
+    var incorrect = ['','','','','']
+
+    for (var i = 0; i < 5; i++){
+        if (data[i] == "yellow"){
+            includes.push(letters[i])
+            incorrect[i] = letters[i]
+        } else if (data[i] == "gray"){
+            excludes.push(letters[i])
+        } else if (data[i] == "green"){
+            correct[i] = letters[i]
+        }
+    }
+
+    console.log("Include: " + includes + "\nExclude: " + excludes + "\nCorrect: " + correct + "\nIncorrect: " + incorrect)
+
+    var guesses = []
+
+    var wordlist = []
+    if (words == 1){
+        wordlist = allwords
+    } else {
+        wordlist = words
+    }
+
+    wordlist.forEach((word) => {
+
+        var valid = true
+        var charList = []
+
+        // For every letter in the word
+        for (var i = 0; i < 5; i++){
+
+            charList.push(word[i])
+
+            // If the letter is in the excludes list - WRONG
+            if (excludes.includes(word[i])){
+                valid = false
+            }
+
+            // If the ith letter doesn't match correct - WRONG
+            if (correct[i] != "" && correct[i] != word[i]){
+                valid = false
+            }
+
+            // If the ith letter matches one of the incorrect letters - WRONG
+            if (incorrect[i] == word[i]){
+                    valid = false
+            }
+
+        }
+
+        // If it does not include every character in includes
+        if (!includes.every(v => charList.includes(v))){
+            valid = false
+        }
+
+        if (valid) {
+            guesses.push(word)
+        }
+    })
+    return guesses
+}
+
+          /*
+function sortG(guesses) {
+    pairs = {}
+    for word in guesses:
+        pairs.update({word : sum(SCORES[letter] for letter in word)})
+    return sorted(pairs.items(), key=lambda x: x[1])
+}
+
+function uniqueLetters(guesses){
+    var temp = []
+    for (word in guesses){
+        var letters_seen = []
+        for (letter in word){
+            if not(letter in letters_seen){
+                letters_seen.append(letter)
+                app = True
+            }else {
+                app = False
+                break
+            }
+        }
+        if (app) {
+            temp.append(word)
+        }
+    }
+    return temp
+} */

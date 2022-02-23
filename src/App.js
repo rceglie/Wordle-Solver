c05e7c6c2a80c86650851f0d2e09374fa0a0e233
Robert Ceglie <rceglie@email.unc.edu>
2022-02-22 19:35:20 -0500

    gray :(

--- src/App.js
+++ src/App.js
@@ -7,27 +7,39 @@ function App() {
 
   const rows = [0,1,2,3,4]
 
-  var guess = 0
+  var guessnum = 0
   var letternum = 0
+
+  const updateColors = () => {
+    for (var i = 0; i < 5; i++){
+      var btn = document.getElementsByClassName(`row-${guessnum} letter-${i}`).item(0)
+      btn.style.backgroundColor = btn.getAttribute("data-color");
+      btn.style.color = "white"
+    }
+  }
+
+  function guess () {
+    updateColors()
+  }
 
   document.addEventListener('keydown', function(event) {
     if (/^[A-Za-z]$/.test(event.key) && letternum < 5){
-      document.getElementsByClassName(`row-${guess} letter-${letternum}`).item(0).textContent = event.key
+      document.getElementsByClassName(`row-${guessnum} letter-${letternum}`).item(0).textContent = event.key
       letternum++
     }
     if (letternum > 0 && event.key === "Backspace"){
-      document.getElementsByClassName(`row-${guess} letter-${letternum-1}`).item(0).textContent = ""
+      document.getElementsByClassName(`row-${guessnum} letter-${letternum-1}`).item(0).textContent = ""
       letternum--
     }
   })
 
   return (
     <div className="App">
-      <div className="title">
-        <h3 className="title">Title</h3>
-      </div>
       <div className="letter-area">
         {rows.map((row) => <LetterRow row={`${row}`}/>)}
+        <div className="guess-area">
+          <button className="guess-btn" onClick={guess}>Guess</button>
+        </div>
       </div>
     </div>
     // Initialize stuff

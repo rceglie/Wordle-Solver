7b54511295c58576d2a648097512fa035397eed5
rceglie <rceglie@email.unc.edu>
2022-02-22 22:28:43 -0500

    starting to get buttons working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import React from 'react'
 
 function App() {
 
-  var letterIsOpen
-
   const rows = [0,1,2,3,4]
 
   var guessnum = 0
@@ -16,7 +14,6 @@ function App() {
       var btn = document.getElementsByClassName(`row-${guessnum} letter-${i}`).item(0)
       btn.style.backgroundColor = btn.getAttribute("data-color");
       btn.style.color = "white"
-      btn.disabled=false
     }
   }
 
@@ -24,48 +21,58 @@ function App() {
     updateColors()
   }
 
-  function letterPressed () {
-    console.log("button pressed")
+  function letterClick (row, letter) {
+    console.log(row, letter)
+    /*btn = ""
+    if (btn.style.color = "grey"){
+      btn.style.color = "yellow"
+    } else if (btn.style.color = "yellow"){
+      btn.style.color = "green"
+    } else {
+      btn.style.backgroundColor = "grey"
+    }*/
   }
 
   document.addEventListener('keydown', function(event) {
     if (/^[A-Za-z]$/.test(event.key) && letternum < 5){
-      document.getElementsByClassName(`row-${guessnum} letter-${letternum}`).item(0).textContent = event.key
+      var btn = document.getElementsByClassName(`row-${guessnum} letter-${letternum}`).item(0)
+      btn.textContent = event.key
+      btn.removeAttribute("disabled")
       letternum++
     }
     if (letternum > 0 && event.key === "Backspace"){
-      document.getElementsByClassName(`row-${guessnum} letter-${letternum-1}`).item(0).textContent = ""
+      var btn = document.getElementsByClassName(`row-${guessnum} letter-${letternum-1}`).item(0)
+      btn.textContent = ""
+      btn.setAttribute("disabled", "")
       letternum--
     }
   })
+
+  const LetterRow  = (row)  => {
+
+    var letters = [0,1,2,3,4]
+
+    return(
+      <div className="letter-row">
+        {letters.map((letter) => <button 
+          className={`letter row-${row} letter-${letter}`}
+          data-color="grey"
+          onClick={()=>letterClick.call(row, letter)}
+        ></button>)}
+      </div>
+    )
+  }
 
   return (
     <div className="App">
       <div className="letter-area">
         {rows.map((row) => LetterRow(row))}
         <div className="guess-area">
-          <button className="guess-btn" onClick={guess}>Guess</button>
+          <button className="guess-btn" onClick={guess}>Find Words</button>
         </div>
       </div>
     </div>
-    // Initialize stuff
   );
-}
-
-
-function LetterRow (row) {
-
-  const letters = [0,1,2,3,4]
-
-  return(
-      <div className="letter-row">
-          {letters.map((letter) => <button 
-              className={`letter row-${row} letter-${letter}`}
-              data-color="grey"
-              disabled
-              ></button>)}
-      </div>
-  )
 }
 
 export default App;
